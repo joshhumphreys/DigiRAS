935eea577dc6da0f854fe9298c8322a78fc284e3
Josh Humphreys <109291625+joshhumphreys@users.noreply.github.com>
2022-07-19 17:20:19 +0100

    excess journey added

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Run this code when a form is submitted to 'excess_question_answer'
+router.post('/excess_question_answer', function (req, res) {
+
+  // Make a variable and give it the value from 'excess_question'
+  var yesorno = req.session.data['excess_question']
+
+  // Check whether the variable matches a condition
+  if (yesorno == "No"){
+    // Send user to 'no excess' flow
+    res.redirect('interim_claim/no_excess/reference_question')
+  } else {
+    // Send user to 'yes excess' flow
+    res.redirect('interim_claim/yes_excess/reference_question_excess')
+  }
+
+})
+
 module.exports = router
